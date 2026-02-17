dfa201d6bd5313dc5d82f2b0159cf142b0e03052
Carlos Gaton <cgaton@uoc.edu>
2026-02-17 16:07:04 +0100

    Cleaning repository

--- src/utils.py
+++ src/utils.py
@@ -1,15 +1,46 @@
+"""
+Utility functions for data loading and saving.
+
+This module provides helper functions to read and write CSV files
+used throughout the TCGA-PAAD survival modeling project.
+"""
+
 import pandas as pd
-import numpy as np
 
 
 def read_data(path, sep=','):
-    """ Recibe un path y un separador y devuelve un dataframe. """
+    """
+    Load a CSV file into a pandas DataFrame.
+
+    Parameters
+    ----------
+    path : str or Path
+        Path to the CSV file.
+    sep : str, optional
+        Field delimiter for the CSV file (default is ',').
+
+    Returns
+    -------
+    pd.DataFrame
+        Loaded DataFrame.
+    """
 
     return pd.read_csv(path, sep=sep)
-    
 
 def save_data(df, path):
-    """ Recibe un dataframe y un path y guarda el dataframe en un archivo csv.
+    """
+    Save a pandas DataFrame to a CSV file.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        DataFrame to save.
+    path : str or Path
+        Destination file path.
+
+    Returns
+    -------
+    None
     """
 
     df.to_csv(path, index=False)
